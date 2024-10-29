a1fb3a4d3fe154d1e780bb2fb7001a6d4e3560a9
Ric Rodriguez <rodriguez.ric@gmail.com>
2024-10-28 20:09:48 -0400

    master

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1730159943|1368368573';
+const CACHE_VERSION = '1730160578|2003317710';
 /** @type {string} */
 const CACHE_PREFIX = 'Raven Quill-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
